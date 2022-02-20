1435ef1788a741484e6cdcfd46a124470ca6feb9
Duncan kariuki <duncankariuki60@gmail.com>
2022-02-20 10:45:22 +0300

    Setup tab bar as the root view, add home, download, upcoming and search views

---
+++ Film Forever/Controllers/DownloadsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadsViewController.swift
+//  Film Forever
+//
+//  Created by Duncan K on 20/02/2022.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Film Forever/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Film Forever
+//
+//  Created by Duncan K on 20/02/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+ 
